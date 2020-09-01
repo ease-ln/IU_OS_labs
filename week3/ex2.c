a16876c1f7b37fe82bacb8c1a1a94bc66a1be49f
ease-ln <69844629+ease-ln@users.noreply.github.com>
2020-09-02 01:23:14 +0600

    Update ex2.c

--- week3/ex2.c
+++ week3/ex2.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
 void bubble_sort(int[], int);
-
 int main(){
-
 int arr[100], n;
 
 printf("Enter number of elements\n");
 scanf("%ld", &n);
-
 printf("Enter %ld integers\n", n);
-
 for (int c = 0; c < n; c++)
     scanf("%d", &arr[c]);
 
 bubble_sort(arr, n);
 
-  printf("Sorted array:\n");
-
-
+printf("Sorted array:\n");
 for (int i = 0; i < n; i++)
      printf("%d\n", arr[i]);
 
- return 0;
+return 0;
 }
+
 void bubble_sort(int arr[], int n){
 int temp;
 for(int i = 0; i< n-1; i++){
