12dfc80e9f88ab49c1804825707b18797cb4a71a
ease-ln <69844629+ease-ln@users.noreply.github.com>
2020-09-01 16:22:24 +0600

    Update ex1.c

--- week2/ex1.c
+++ week2/ex1.c
@@ -4,17 +4,20 @@
 
 int main(void)
 {
- int Integermax;
- float Floatmax;
- double Doublemax;
+    int Integermax;
+    float Floatmax;
+    double Doublemax;
 
-  Integermax = INT_MAX;
-  Floatmax = FLT_MAX;
-  Doublemax = DBL_MAX;
+    Integermax = INT_MAX;
+    Floatmax = FLT_MAX;
+    Doublemax = DBL_MAX;
 
-printf("INT MAX %d\n", Integermax);
-printf("FLT_MAX %f\n", Floatmax);
-printf("DBL MAX %f\n", Doublemax);
+    printf("INT MAX %d\n", Integermax);
+    printf("FLT_MAX %f\n", Floatmax);
+    printf("DBL MAX %f\n", Doublemax);
+    printf("INT MAX %lu\n", sizeof(Integermax));
+    printf("FLT_MAX %lu\n", sizeof(Floatmax));
+    printf("FLT_MAX %lu\n", sizeof(Doublemax));
 
- return 0;
+    return 0;
 }
