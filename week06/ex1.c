80401656a0d77fca7185c3e653185851e6a9d9dc
ease-ln <poiuyt1545>
2020-09-23 17:17:29 +0600

    week06

---
+++ week06/ex1.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* First come, first served implementation
+id  = process ID
+at  = arrival time
+bt  = burst time
+ct  = complete time
+wt  = waiting time
+tat = turnaround time
+atat = average turnaround time
+awt  = average waiting time
+Algorithm:  1. Sort stable processes by arrival time
+            2. Compute ct, wt, tat, atat, awt
+Example used: n = 3 at/bt: 2/4 3/6 1/4
+*/
+//process structure
+struct process{
+   int id;
+   int at;
+   int bt;
+   int ct;
+   int wt;
+   int tat;
+};
+//comparator for stable quicksort
+int comparator(const void *p, const void *q)
+{
+    struct process *pa = (struct process *) p;
+    struct process *pb = (struct process *) q;
+    int res = pa->at - pb->at;
+    return res != 0 ? res : pa->id - pb->id;
+}
+/*We assume that the n, at, bt positive integer
+ numbers by task conditions and lecture examples*/
+int main(){
+    int timer = 0;
+
+    int n = 0;
+    printf("Enter number of processes:");
+    scanf("%d", &n);
+    //check whatever n is positive or not
+    /*if(n<0){
+        printf("n can not be less than 0");
+        return 0;
+    }*/
+
+    printf("Enter Arrival(AT) and Burst(BT) time for each process:\n");
+    struct process pro[n];
+
+    for(int i = 0; i < n; ++i){
+    pro[i].id = i+1;
+    printf("AT of process[%d]:", i);
+    scanf("%d", &pro[i].at);
+        //check whatever at is positive or not
+        /*if(pro[i].at<0){
+        printf("AT can not be less than 0");
+        return 0;
+        }*/
+    printf("BT of process[%d]:", i);
+    scanf("%d", &pro[i].bt);
+        //check whatever bt is positive or not
+        /*if(pro[i].bt<0){
+        printf("BT can not be less than 0");
+        return 0;
+        }*/
+    }
+    //stable quick sort of processes by pro[i].at
+    qsort(pro, n, sizeof(struct process), comparator);
+
+    timer = pro[0].at;
+
+    float atat = 0;
+    float awt = 0;
+    //Computing ct, wt, tat, atat, awt
+    for (int i = 0; i < n; ++i){
+            pro[i].ct = timer+pro[i].bt;
+            timer = pro[i].ct;
+            pro[i].tat = pro[i].ct - pro[i].at;
+            pro[i].wt  = pro[i].tat -pro[i].bt;
+            atat = atat + pro[i].tat;
+            awt = awt + pro[i].wt;
+
+    }
+    //Result printing
+    printf("ID\t    AT\t    BT\t    CT\t    TAT\t    WT\t\n");
+    for (int i = 0; i < n; ++i)
+    printf("%d\t     %d\t     %d\t     %d\t     %d\t      %d\t\n", pro[i].id, pro[i].at, pro[i].bt, pro[i].ct, pro[i].tat, pro[i].wt);
+    printf("Average Turnaround time: %f\n", atat/n);
+    printf("Average Waiting time: %f\n", awt/n
+           );
+    return 0;
+}
---
+++ week06/ex3.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+the average turnaround and waiting time in round robin
+ is less comparing to the FCFS and SJF
+*/
+
+/* Round robin implementation
+q   = quantum
+id  = process ID
+at  = arrival time
+bt  = burst time
+ct  = complete time = exit time
+wt  = waiting time
+tat = turnaround time
+rm  = remaining burst time
+atat = average turnaround time
+awt  = average waiting time
+Algorithm:  1. Sort stable processes by arrival time with respect to id
+            2. Compute et
+            3. Compute wt, tat, atat, awt
+Example used: n = 3 at/bt: 2/4 3/6 1/4
+*/
+struct process{
+   int id;
+   int at;
+   int bt;
+   int ct;
+   int wt;
+   int tat;
+   int rm;
+};
+//Comparator for qsort by arrival time with respect to id
+int comparator(const void *p, const void *q)
+{
+    struct process *pa = (struct process *) p;
+    struct process *pb = (struct process *) q;
+    int res = pa->at - pb->at;
+    return res != 0 ? res : pa->id - pb->id;
+}
+/*We assume that the q, n, at, bt positive integer
+ numbers by task conditions and lecture examples*/
+int main(){
+    int n = 0;
+    int quan = 0;
+    float atat = 0;
+    float awt = 0;
+    //q[n] represent the queue
+    int q[n];
+
+    printf("Enter number of processes:");
+    scanf("%d", &n);
+
+    printf("Enter quantum:");
+    scanf("%d", &quan);
+
+    /*if(n<0){
+        printf("n can not be less than 0");
+        return 0;
+    }*/
+
+    printf("Enter Arrival(AT) and Burst(BT) time for each process:\n");
+    struct process pro[n];
+
+    for(int i = 0; i < n; ++i){
+    pro[i].id = i+1;
+    printf("AT of process[%d]:", i);
+    scanf("%d", &pro[i].at);
+        if(pro[i].at<0){
+        printf("AT can not be less than 0");
+        return 0;
+        }
+    printf("BT of process[%d]:", i);
+    scanf("%d", &pro[i].bt);
+    pro[i].rm = pro[i].bt;
+        if(pro[i].bt<0){
+        printf("BT can not be less than 0");
+        return 0;
+        }
+    }
+    //qsort by arrival time with respect to id
+    qsort(pro, n, sizeof(struct process), comparator);
+
+    //Initialize every element of q to -1
+    for(int i = 0; i < n; i++){
+        q[i] = -1;
+    }
+
+    //Simulation of round robin process
+   int temp=0;
+   int t = pro[0].at;
+    int tq = 0;
+    while(tq !=1){
+            tq = 1;
+        for(int i = 0; i < n; i++){
+            if(t >= pro[i].at && pro[i].rm>0){
+                if(pro[i].rm>quan){
+                    temp = quan;
+                } else {
+                    temp = pro[i].rm;
+                }
+                pro[i].rm-=temp;
+                q[i] = pro[i].rm;
+                t+=temp;
+                if(q[i]==0){
+                pro[i].ct = t;
+                pro[i].tat = pro[i].ct - pro[i].at;
+                pro[i].wt  = pro[i].tat -pro[i].bt;
+                atat = atat + pro[i].tat;
+                awt = awt + pro[i].wt;
+                }
+
+            } else if(q[i]>0){
+                pro[i].rm-=temp;
+                q[i] = pro[i].rm;
+                t+=temp;
+                if(q[i]==0){
+                pro[i].ct = t;
+                pro[i].tat = pro[i].ct - pro[i].at;
+                pro[i].wt  = pro[i].tat -pro[i].bt;
+                atat = atat + pro[i].tat;
+                awt = awt + pro[i].wt;
+                }
+            } else if(q[i]<=0){
+                continue;
+            }
+            for(int k = 0; k < n; k++){
+                if(q[k]!=0){tq=0;}
+            }
+        }
+    }
+
+
+//Result printing
+    printf("ID\t    AT\t    BT\t    CT\t    TAT\t    WT\t\n");
+    for (int i = 0; i < n; ++i)
+    printf("%d\t     %d\t     %d\t     %d\t     %d\t      %d\t\n", pro[i].id, pro[i].at, pro[i].bt, pro[i].ct, pro[i].tat, pro[i].wt);
+    printf("Average Turnaround time: %f\n", atat/n);
+    printf("Average Waiting time: %f\n", awt/n
+           );
+    return 0;
+}
