7dcb3234c50d3b243d8c5f3233bad7586398412a
ease-ln <69844629+ease-ln@users.noreply.github.com>
2020-10-27 17:16:17 +0600

    Update ex4.c

--- week11/ex4.c
+++ week11/ex4.c
@@ -6,21 +6,21 @@
 
 int main() {
 	struct stat src_st;
-    struct stat dest_st;
+	struct stat dest_st;
 
 	int SRC = open("ex1.txt", O_RDWR);
-    int DEST = open("ex1.memcpy.txt", O_RDWR);
+	int DEST = open("ex1.memcpy.txt", O_RDWR);
 
 	fstat(SRC, &src_st);
-    fstat(DEST, &dest_st);
+	fstat(DEST, &dest_st);
 
 	char *mapping_src = mmap(NULL, src_st.st_size, PROT_READ, MAP_SHARED, SRC, 0);
 	char *mapping_dest = mmap(NULL, dest_st.st_size, PROT_WRITE, MAP_SHARED, DEST, 0);
 
-    ftruncate(DEST, strlen(mapping_src)*sizeof(char));
+	ftruncate(DEST, strlen(mapping_src)*sizeof(char));
 	memcpy(mapping_dest, mapping_src, src_st.st_size);
 	close(SRC);
-    close(DEST);
+	close(DEST);
 
 	return 0;
 }
